c8b7f621d51d417a3dc4776af58f1faa7ad7cd96
isra el <vernu1997@gmail.com>
2023-10-21 21:30:26 +0300

    chore(fe): add card hover effect

--- frontend/src/features/MovieSearchResult/components/MovieGridCard.tsx
+++ frontend/src/features/MovieSearchResult/components/MovieGridCard.tsx
@@ -10,7 +10,7 @@ export default function MovieGridCard({ movie }: MovieGridCardProps) {
   const { id, image, title, genres, rating, duration, year } = movie
 
   return (
-    <Link to={`/movies/${id}`}>
+    <Link to={`/movies/${id}`} className='hover:scale-105 duration-300'>
       <div className='flex flex-col gap-1'>
         <div className='relative'>
           <img src={image} alt={title} className='rounded-xl' />
--- frontend/src/features/MovieSearchResult/components/MovieListCard.tsx
+++ frontend/src/features/MovieSearchResult/components/MovieListCard.tsx
@@ -10,7 +10,7 @@ export default function MovieListCard({ movie }: MovieListCardProps) {
   const { id, image, title, genres, rating, duration, year } = movie
 
   return (
-    <Link to={`/movies/${id}`}>
+    <Link to={`/movies/${id}`} className='hover:scale-105 duration-300'>
       <div className='grid grid-cols-3 lg:grid-cols-4'>
         <div className=''>
           <img src={image} alt={title} className='h-32 lg:h-48' />
