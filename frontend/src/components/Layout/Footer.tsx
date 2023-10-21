ca25b293171acedd985b4107aef838611b7137df
isra el <vernu1997@gmail.com>
2023-10-21 18:32:02 +0300

    chore(fe): build  header and footer components

--- frontend/src/components/Layout/Footer.tsx
+++ frontend/src/components/Layout/Footer.tsx
@@ -1,3 +1,57 @@
+import footerLogo from '@/assets/logos/footer-logo.svg'
+import DribbbleIcon from '../icons/DribbbleIcon'
+import { Link } from 'react-router-dom'
+import FacebookIcon from '../icons/FacebookIcon'
+import InstagramIcon from '../icons/InstagramIcon'
+import LinkedInIcon from '../icons/LinkedInIcon'
+
+const socialIcons = [
+  {
+    icon: <FacebookIcon />,
+    link: '#',
+  },
+  {
+    icon: <LinkedInIcon />,
+    link: '#',
+  },
+  {
+    icon: <DribbbleIcon />,
+    link: '#',
+  },
+  {
+    icon: <InstagramIcon />,
+    link: '#',
+  },
+]
+
 export default function Footer() {
-  return <footer className='mt-auto'>Footer</footer>
+  return (
+    <footer className='mt-auto mx-12 my-4 '>
+      <div className='flex justify-between'>
+        <div className='flex flex-row justify-center items-center font-thin'>
+          <img src={footerLogo} alt='HelloMovies Logo' className='w-32 h-4' />
+          <span className='text-sky-300 text-xl mr-4'>|</span>
+
+          <span className=''>
+            &copy; Copyright {new Date().getFullYear()} HelloMovies. All rights
+            reserved.
+          </span>
+        </div>
+
+        <div className=''>
+          {
+            <div className='flex flex-row gap-4'>
+              {socialIcons.map(({ icon, link }) => (
+                <span className='w-5'>
+                  <Link to={link}>
+                    <span className='w-4 fill-light'> {icon}</span>
+                  </Link>
+                </span>
+              ))}
+            </div>
+          }
+        </div>
+      </div>
+    </footer>
+  )
 }
--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,9 +1,16 @@
 import { Link } from 'react-router-dom'
+import headerLogo from '@/assets/logos/header-logo.svg'
 
 export default function Header() {
   return (
     <header>
-      <Link to='/'>HelloMovies</Link>
+      <Link to='/'>
+        <img
+          src={headerLogo}
+          alt='HelloMovies Logo'
+          className='w-32 mx-12 my-4'
+        />
+      </Link>
     </header>
   )
 }
